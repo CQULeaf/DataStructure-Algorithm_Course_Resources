da78813f7235eeb9c390102e01a4b36cc004b947
Xuhang Ye <609388896@qq.com>
2023-12-21 18:26:40 +0800

    finish the Selection Sort section

--- Class-Contents-Only-by-C++/Algorithms/Sort/Insertion Sort/Binary Insertion Sort.cpp
+++ Class-Contents-Only-by-C++/Algorithms/Sort/Insertion Sort/Binary Insertion Sort.cpp
@@ -7,6 +7,50 @@ void BinaryInsertionSort(vector<int>& arr) {
     int n = arr.size();
     for (int i = 1; i < n; i++)
     {
-        
+        int key = arr[i];
+        int left = 0;
+        int right = i - 1;
+
+        // 使用二分查找定位插入位置
+        while (left <= right) {
+            int mid = (left + right)/2;
+            if (arr[mid] > key) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        // 移动元素并插入
+        for (int j = i - 1; j >= left; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[left] = key;
     }
+}
+
+int main() {
+    vector<int> arr;
+    int n, element;
+
+    cout << "请输入元素的数量：";
+    cin >> n;
+
+    cout << "请输入 " << n << " 个元素：";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> element;
+        arr.push_back(element);
+    }
+
+    BinaryInsertionSort(arr);
+
+    cout << "排序后的数组：";
+    for (int i = 0; i < n; i ++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    system("pause");
+    return 0;
 }
---
+++ Class-Contents-Only-by-C++/Algorithms/Sort/Insertion Sort/Shell Sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void ShellSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        // 对每个子序列进行直接插入排序
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                arr[j] = arr[j - gap];
+            arr[j] = temp;
+        }
+    }
+}
+
+int main() {
+    vector<int> arr;
+    int n, element;
+
+    cout << "请输入元素的数量：";
+    cin >> n;
+
+    cout << "请输入 " << n << " 个元素：";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> element;
+        arr.push_back(element);
+    }
+
+    ShellSort(arr);
+
+    cout << "排序后的数组：";
+    for (int i = 0; i < n; i ++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
