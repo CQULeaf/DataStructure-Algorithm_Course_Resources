303c82c01b21da53493c3e34d9d21a0f9325a70f
Xuhang Ye <609388896@qq.com>
2023-12-26 00:55:02 +0800

    fix something bad

--- Homework-by-C++/Homework-Week-4/Hw 4_1 字符串匹配问题.cpp
+++ Homework-by-C++/Homework-Week-4/Hw 4_1 字符串匹配问题.cpp
@@ -4,18 +4,20 @@
 #include <string>
 #include <vector>
 
-std::string is_valid(const std::string &s) {
-    std::stack<char> stack;
-    std::unordered_map<char, char> mapping = {
+using namespace std;
+
+string is_valid(const string& s) {
+    stack<char> stack;
+    unordered_map<char, char> mapping = {
         {')', '('},
         {']', '['},
         {'}', '{'},
         {'>', '<'}
     };
-    std::string order = "<([{";
+    string order = "<([{";
 
     for (char charact : s) {
-        if (order.find(charact) != std::string::npos) {
+        if (order.find(charact) != string::npos) {
             if (!stack.empty() && order.find(stack.top()) < order.find(charact))
                 return "NO";
             stack.push(charact);
@@ -33,17 +35,17 @@ std::string is_valid(const std::string &s) {
 
 int main() {
     int n;
-    std::cin >> n;
-    std::vector<std::string> results;
+    cin >> n;
+    vector<string> results;
 
     for (int i = 0; i < n; ++i) {
-        std::string s;
-        std::cin >> s;
+        string s;
+        cin >> s;
         results.push_back(is_valid(s));
     }
 
     for (const auto &res : results)
-        std::cout << res << std::endl;
+        cout << res << endl;
 
     return 0;
 }
--- Homework-by-C++/Homework-Week-4/Hw 4_2 后缀表达式求值.cpp
+++ Homework-by-C++/Homework-Week-4/Hw 4_2 后缀表达式求值.cpp
@@ -3,20 +3,22 @@
 #include <string>
 #include <stack>
 
-int evaluate_postfix(const std::string& expression) {
-    std::stack<int> stack;
+using namespace std;
 
-    std::istringstream iss(expression);
-    std::string token;
+int evaluate_postfix(const string& expression) {
+    stack<int> stack;
+
+    istringstream iss(expression);
+    tring token;
 
     while (iss >> token) {
         if (isdigit(token[0]) || (token[0] == '-' && token.size() > 1 && isdigit(token[1]))) //This checks if token starts with a negative number
         {
-            stack.push(std::stoi(token));
+            stack.push(stoi(token));
         } 
         else {
             if (stack.size() < 2) {
-                std::cout << "Expression Error: " << (stack.empty() ? 0 : stack.top()) << std::endl;
+                cout << "Expression Error: " << (stack.empty() ? 0 : stack.top()) << endl;
                 return -1;  // Using -1 as an error code
             }
 
@@ -31,7 +33,7 @@ int evaluate_postfix(const std::string& expression) {
                 stack.push(a * b);
             } else if (token == "/") {
                 if (b == 0) {
-                    std::cout << "Error: " << a << "/0" << std::endl;
+                    cout << "Error: " << a << "/0" << std::endl;
                     return -1;
                 }
                 stack.push(a / b);
@@ -40,7 +42,7 @@ int evaluate_postfix(const std::string& expression) {
     }
 
     if (stack.size() != 1) {
-        std::cout << "Expression Error: " << stack.top() << std::endl;
+        cout << "Expression Error: " << stack.top() << endl;
         return -1;
     }
 
@@ -48,13 +50,13 @@ int evaluate_postfix(const std::string& expression) {
 }
 
 int main() {
-    std::string input_line;
-    std::getline(std::cin, input_line);
+    string input_line;
+    getline(cin, input_line);
     input_line.erase(input_line.find('#'));  // Remove the '#' character
 
     int result = evaluate_postfix(input_line);
     if (result != -1) {
-        std::cout << result << std::endl;
+        cout << result << endl;
     }
 
     return 0;
