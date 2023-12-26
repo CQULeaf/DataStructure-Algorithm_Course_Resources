e5b95c0a394dc5324f7c11f1e2b6f6626359b1c2
Xuhang Ye <609388896@qq.com>
2023-12-26 12:44:45 +0800

    fix some bugs

--- Homework-by-C++/Homework-Week-4/Hw 4_2 后缀表达式求值.cpp
+++ Homework-by-C++/Homework-Week-4/Hw 4_2 后缀表达式求值.cpp
@@ -1,63 +1,56 @@
 #include <iostream>
-#include <sstream>
-#include <string>
 #include <stack>
 
 using namespace std;
 
-int evaluate_postfix(const string& expression) {
-    stack<int> stack;
+int evaluate_postfix(stack<int>& s, const string op) {
+    int second = s.top(); s.pop();
+    int first = s.top(); s.pop();
 
-    istringstream iss(expression);
-    tring token;
-
-    while (iss >> token) {
-        if (isdigit(token[0]) || (token[0] == '-' && token.size() > 1 && isdigit(token[1]))) //This checks if token starts with a negative number
-        {
-            stack.push(stoi(token));
-        } 
-        else {
-            if (stack.size() < 2) {
-                cout << "Expression Error: " << (stack.empty() ? 0 : stack.top()) << endl;
-                return -1;  // Using -1 as an error code
-            }
-
-            int b = stack.top(); stack.pop();
-            int a = stack.top(); stack.pop();
-
-            if (token == "+") {
-                stack.push(a + b);
-            } else if (token == "-") {
-                stack.push(a - b);
-            } else if (token == "*") {
-                stack.push(a * b);
-            } else if (token == "/") {
-                if (b == 0) {
-                    cout << "Error: " << a << "/0" << std::endl;
-                    return -1;
-                }
-                stack.push(a / b);
-            }
+    if (op == "+") s.push(first + second);
+    else if (op == "-") s.push(first - second);
+    else if (op == "*") s.push(first * second);
+    else if (op == "/") {
+        if (second == 0) {
+            cout << "Error: " << first << "/0" << endl;
+            return -1;
         }
+        s.push(first / second);
     }
 
-    if (stack.size() != 1) {
-        cout << "Expression Error: " << stack.top() << endl;
-        return -1;
-    }
-
-    return stack.top();
+    return 0;
 }
 
 int main() {
-    string input_line;
-    getline(cin, input_line);
-    input_line.erase(input_line.find('#'));  // Remove the '#' character
+    stack<int> s;
+    string token;
 
-    int result = evaluate_postfix(input_line);
-    if (result != -1) {
-        cout << result << endl;
+    while (cin >> token) {
+        if (token == "#") break;
+
+        if (isdigit(token[0]) || (token[0] == '-' && token.size() > 1)) {
+            s.push(stoi(token));
+        } else {
+            if (s.size() < 2) {
+                cout << "Expression Error: " << (s.empty() ? 0 : s.top()) << endl;
+            }
+            if (evaluate_postfix(s, token) == -1) {
+                system("pause");
+                return -1;
+            }
+        }
+
     }
 
+    if (s.size() != 1) {
+        cout << "Expression Error: " << s.top() << endl;
+        
+        system("pause");
+        return -1;
+    }
+
+    cout << s.top() << endl;
+
+    system("pause");
     return 0;
 }
