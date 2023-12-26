c58ac17d1f3ac4757c0cd4ec7d305c0f5c3eb393
Xuhang Ye <609388896@qq.com>
2023-12-26 22:20:38 +0800

    complete the quicksort code

--- Class-Contents-Only-by-C++/Algorithms/Sort/Exchange Sort/Quick Sort.cpp
+++ Class-Contents-Only-by-C++/Algorithms/Sort/Exchange Sort/Quick Sort.cpp
@@ -3,8 +3,51 @@
 
 using namespace std;
 
-int Divide(vector<int>& arr, int low, int high) {
-    int pivot = arr[high];
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high]; // Choose the last element as the pivot (always)
+    int i = low - 1;
 
-    
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    vector<int> arr;
+    int n, element;
+
+    cout << "请输入元素的数量：";
+    cin >> n;
+
+    cout << "请输入 " << n << " 个元素：";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> element;
+        arr.push_back(element);
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "排序后的数组：";
+    for (int i = 0; i < n; i ++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    system("pause");
+    return 0;
 }
